f17fe4cd61a941d276a86e935be634caac3e755b
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-06 20:28:55 +0100

    added logic to show contents navigation when the hamburger icon is clicked

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 window.onload = function(event) {
 	const copyrightYear = document.getElementById("copyright-year");
 	copyrightYear.textContent = (new Date()).getFullYear();
+
+	const hamburger = document.getElementById("hamburger");
+	hamburger.addEventListener("click", function(event) {
+		const contents = document.getElementById("contents");
+		if (Array.from(contents.classList).indexOf("contents-open") === -1) {
+			contents.classList.add("contents-open");
+		} else {
+			contents.classList.remove("contents-open");
+		}
+	});
 };
