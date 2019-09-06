bbf1f3c62f6b1dd83afa4c6115ae99c581bb9909
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-06 20:32:30 +0100

    added logic to toggle contents navigation on key down events

--- js/app.js
+++ js/app.js
@@ -3,12 +3,20 @@ window.onload = function(event) {
 	copyrightYear.textContent = (new Date()).getFullYear();
 
 	const hamburger = document.getElementById("hamburger");
-	hamburger.addEventListener("click", function(event) {
+	hamburger.addEventListener("click", toggleContentsNavigation);
+	hamburger.addEventListener("keydown", function(event) {
+		if (event.keyCode === 13 || event.keyCode === 32) {
+			toggleContentsNavigation();
+		} 
+	});
+
+	function toggleContentsNavigation() {
 		const contents = document.getElementById("contents");
+
 		if (Array.from(contents.classList).indexOf("contents-open") === -1) {
 			contents.classList.add("contents-open");
 		} else {
 			contents.classList.remove("contents-open");
 		}
-	});
+	}
 };
