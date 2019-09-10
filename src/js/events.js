883489c39b8a59d76606850b1fa09f478495ab83
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-10 18:02:37 +0100

    changed es6 syntax to es5
    gulp uglify currently does not work with es6 syntax

--- src/js/events.js
+++ src/js/events.js
@@ -1,14 +1,14 @@
 window.onload = function(event) {
-	const hamburger = document.getElementById("hamburger");
+	var hamburger = document.getElementById("hamburger");
 	hamburger.addEventListener("click", toggleContentsNavigation);
 	hamburger.addEventListener("keydown", function(event) {
 		if (event.keyCode === 13 || event.keyCode === 32) {
 			toggleContentsNavigation();
-		} 
+		}
 	});
-	
+
 	function toggleContentsNavigation() {
-		const contents = document.getElementById("contents");
+		var contents = document.getElementById("contents");
 
 		if (Array.from(contents.classList).indexOf("contents-open") === -1) {
 			contents.classList.add("contents-open");
--- src/js/time.js
+++ src/js/time.js
@@ -1,4 +1,4 @@
 window.onload = function(event) {
-	const copyrightYear = document.getElementById("copyright-year");
+	var copyrightYear = document.getElementById("copyright-year");
 	copyrightYear.textContent = (new Date()).getFullYear();
 };
