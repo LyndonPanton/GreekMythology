99dc13e52a0baa1bde11a366985615eaa586a877
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-10 17:06:37 +0100

    moved files and folders to source directories

---
+++ src/README.md
@@ -0,0 +1,7 @@
+# Greek Mythology
+
+## How To Open
+> 1. Go to the project's download folder
+> 2. Right click on the file named _index.html_
+> 3. Choose the _open with_ option
+> 4. Open the project in your desired browser
---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+window.onload = function(event) {
+	const copyrightYear = document.getElementById("copyright-year");
+	copyrightYear.textContent = (new Date()).getFullYear();
+
+	const hamburger = document.getElementById("hamburger");
+	hamburger.addEventListener("click", toggleContentsNavigation);
+	hamburger.addEventListener("keydown", function(event) {
+		if (event.keyCode === 13 || event.keyCode === 32) {
+			toggleContentsNavigation();
+		} 
+	});
+
+	function toggleContentsNavigation() {
+		const contents = document.getElementById("contents");
+
+		if (Array.from(contents.classList).indexOf("contents-open") === -1) {
+			contents.classList.add("contents-open");
+		} else {
+			contents.classList.remove("contents-open");
+		}
+	}
+};
