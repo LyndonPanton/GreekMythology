8c1222b1317a09e674357f9872ab53b3876f7391
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-10 17:37:02 +0100

    added task to compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,18 @@ const concat = require("gulp-concat"); // concatenate files
 
 gulp.task("copyHTML", async function() {
 	gulp.src("src/*.html") // get HTML file(s)
-		.pipe(gulp.dest("build/")); // send to build folder
+		.pipe(gulp.dest("build")); // send to build folder
 });
 
 gulp.task("copyMD", async function() {
 	gulp.src("src/*.md") // get markdown file(s)
-		.pipe(gulp.dest("build/")); // send to build folder
+		.pipe(gulp.dest("build")); // send to build folder
+});
+
+// compile sass
+gulp.task("sass", async function() {
+	gulp.src("src/sass/*.sass") // get sass files
+		.pipe(sass()) // compile files
+		.on("error", sass.logError) // log errors if any
+		.pipe(gulp.dest("build/css")); // send to build folder
 });
