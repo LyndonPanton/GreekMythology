5965d8af344e29276dae33ca084b3d5cff821e9a
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-10 17:20:42 +0100

    required gulp modules

---
+++ gulpfile.js
@@ -0,0 +1,5 @@
+const gulp = require("gulp"); // gulp module
+const imagemin = require("gulp-imagemin"); // optimize images
+const uglify = require("gulp-uglify"); // minify files
+const sass = require("gulp-sass"); // compile sass
+const concat = require("gulp-concat"); // concatenate files
