27ba05d8c3000871496a9ac9f15a93c2de03ebfa
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-10 17:30:03 +0100

    made task asynchronous

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ const uglify = require("gulp-uglify"); // minify files
 const sass = require("gulp-sass"); // compile sass
 const concat = require("gulp-concat"); // concatenate files
 
-gulp.task("copyHTML", function() {
+gulp.task("copyHTML", async function() {
 	gulp.src("src/*.html") // get HTML file(s)
 		.pipe(gulp.dest("build/")); // send to build folder
 });
 
-gulp.task("copyMARK", function() {
+gulp.task("copyMARK", async function() {
 	gulp.src("src/*.md") // get markdown file(s)
 		.pipe(gulp.dest("build/")); // send to build folder
 });
