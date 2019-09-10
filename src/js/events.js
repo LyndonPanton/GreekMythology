401650c6ad2e735400d2548748696a59370cdc61
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-10 17:53:22 +0100

    corrected file
    added event listener

--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,12 @@
 window.onload = function(event) {
+	const hamburger = document.getElementById("hamburger");
+	hamburger.addEventListener("click", toggleContentsNavigation);
+	hamburger.addEventListener("keydown", function(event) {
+		if (event.keyCode === 13 || event.keyCode === 32) {
+			toggleContentsNavigation();
+		} 
+	});
+	
 	function toggleContentsNavigation() {
 		const contents = document.getElementById("contents");
 
