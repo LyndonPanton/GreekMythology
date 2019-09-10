121e174a0fcb5e05cc86e1127c81ef8d86babec7
Lyndon Panton <LyndonPanton@gmail.com>
2019-09-10 17:29:17 +0100

    created task to copy html, markdown files to build folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,13 @@ const imagemin = require("gulp-imagemin"); // optimize images
 const uglify = require("gulp-uglify"); // minify files
 const sass = require("gulp-sass"); // compile sass
 const concat = require("gulp-concat"); // concatenate files
+
+gulp.task("copyHTML", function() {
+	gulp.src("src/*.html") // get HTML file(s)
+		.pipe(gulp.dest("build/")); // send to build folder
+});
+
+gulp.task("copyMARK", function() {
+	gulp.src("src/*.md") // get markdown file(s)
+		.pipe(gulp.dest("build/")); // send to build folder
+});
